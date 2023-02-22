e55c8c8df23122012a6e363779eb90e4a524bb98
ayushgupta-kiwi <ayush.gupta@kiwitech.com>
2023-02-22 13:32:11 +0530

    updated

--- Student_Details/urls.py
+++ Student_Details/urls.py
@@ -1,24 +1,7 @@
 from django.urls import path, include
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-from rest_framework import permissions
 from rest_framework.routers import DefaultRouter
 from . import views
 
-# Swagger Implementation
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Student Authentication",
-        default_version='Student Details',
-        description="This API is created to provide student details to perform CRUD functions for the Authenticated "
-                    "user.",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=[permissions.AllowAny],
-)
 
 # Creating Router Object
 router = DefaultRouter()
@@ -31,6 +14,5 @@ router.register('login', views.StudentLogin, basename='login'),
 router.register('listings', views.UserProfile, basename='listings')
 
 urlpatterns = [
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='swagger'),
     path('student/', include(router.urls)),
 ]
--- Student_Registration/urls.py
+++ Student_Registration/urls.py
@@ -15,8 +15,26 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Student Authentication",
+        default_version='Student Details',
+        description="This API is created to provide student details to perform CRUD functions for the Authenticated "
+                    "user.",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Student_Details.urls')),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='swagger'),
 ]
